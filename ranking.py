7212c67700217f40ec1c8ae97d9ab95465b3a40e
udaykirandhage <105545783+udaykirandhage@users.noreply.github.com>
2023-04-20 23:40:22 +0530

    Update ranking.py

--- ranking.py
+++ ranking.py
@@ -234,3 +234,24 @@ if selectedNavbar =="Home":
             "container": {"padding": "0!important", "background-color": "#fafafa","width":"100vw"},
             "icon": {"color": "orange", "font-size": "10px"}, 
             "nav-link": {"font-size": "15px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"}},)
+if selectedNavbar == "Contact-Us":
+    
+    st.header(":mailbox: Customer Support")
+    contact_form="""
+    <form action="https://formsubmit.co/udaykirandhage@gmail.com" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name"  placeholder="Your Name"required>
+        <input type="email" name="email" placeholder="Your Gmail" required>
+        <input type="text" name="number" placeholder="Your Number" required>
+        <textarea name="Address" placeholder=" Enter Your Address"></textarea>
+        <button type="submit">Send</button>
+    </form> """
+    st.markdown(contact_form,unsafe_allow_html=True)
+
+    def local_css(file_name):
+        with open(file_name) as f:
+            st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
+    local_css("style.css")
+
+
+
